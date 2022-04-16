ab23c3240f9fbd79440fc7fc8629f4733a28e968
David Portador <david.raw172@gmail.com>
2022-04-16 14:21:34 -0500

    Ajuste de estilos personalizados y de bootstrap

--- php/registro.php
+++ php/registro.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Crea tu cuenta</title>
-  <link rel="stylesheet" href="../css/styles.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+		<link rel="shortcut icon" href="../assets/logo_splash.png">
+	<title></title>
 </head>
 <body>
   <div class="signupFrm">
     <div class="wrapper">
-    <form action="" class="form">
+    <form action="" class="form-green">
       <h1 class="title">Registro</h1>
 
       <div class="inputContainer">
-        <input type="text" class="input" placeholder="a">
-        <label for="" class="label">Email</label>
+        <input type="text" class="input-green" placeholder="a">
+        <label for="" class="labelform">Email</label>
       </div>
 
       <div class="inputContainer">
-        <input type="text" class="input" placeholder="a">
-        <label for="" class="label">Username</label>
+        <input type="text" class="input-green" placeholder="a">
+        <label for="" class="labelform">Username</label>
       </div>
 
       <div class="inputContainer">
-        <input type="text" class="input" placeholder="a">
-        <label for="" class="label">Password</label>
+        <input type="text" class="input-green" placeholder="a">
+        <label for="" class="labelform">Password</label>
       </div>
 
       <div class="inputContainer">
-        <input type="text" class="input" placeholder="a">
-        <label for="" class="label">Confirm Password</label>
+        <input type="text" class="input-green" placeholder="a">
+        <label for="" class="labelform">Confirm Password</label>
       </div>
 
       <input type="submit" class="submitBtn" value="Sign up">
